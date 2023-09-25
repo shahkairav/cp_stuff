fe2adf1ef56cbcd1c6eed95571dc934d4932e057
Kairav Shah <kairavshah1702@gmail.com>
2023-09-25 11:35:46 +0530

    added solution to 122. Best Time to Buy and Sell Stock II

---
+++ data_structures/sparse_table.cpp
@@ -0,0 +1,19 @@
+/*
+- Allows answering range queries in O(log N)
+- Its true power lies in answer range-maximum/minimum queries in O(1)
+- However, does not support array updates.
+*/
+
+// Precompute all answers for queries of ranges of lengths that are powers of 2.
+
+class SparseTable {
+private:
+  int K = 25; // reasonable value of log N for N <= 10^7
+
+public:
+
+};
+
+int main() {
+  return 0;
+}
